bf989b7f19c4e909b4f808a067730f718e4f4dc4
e-klein-uscprojects <ethanklein.athlete@gmail.com>
2025-07-22 14:39:14 -0700

    Add flight optimization model logic

--- models/flight_optimizer.py
+++ models/flight_optimizer.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+def optimize_flight(data):
+    data = data.dropna()
+    X = data[['wind_speed', 'air_traffic']]
+    y = data['duration']
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    data['optimized'] = model.predict(X)
+    return data
 
